e10d8d27e091e4d00151226684a2cac5ddfe8ed1
Pasula, Pavan K <pasula_pavan@optum.com>
2022-06-01 13:04:51 +0530

    FIRST

---
+++ src/main/scala/pack/obj.scala
@@ -0,0 +1,29 @@
+package pack
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import sys.process._
+
+object obj {
+  def main(args: Array[String]): Unit = {
+
+
+
+   /* val spark= SparkSession.builder()
+      .master("local[*]")
+      .appName("SparkByExamples.com")
+      .getOrCreate()
+*/
+    /*val data=spark.textFile("C:\\Users\\ppasula1\\Desktop\\test.txt")
+    data.foreach(println)*/
+    println("Started")
+    val a=2
+    println(a)
+    val list=List(1,2,3,4,5,6)
+    val newlis=list.filter(x=>x>3)
+    newlis.foreach(println)
+
+  }
+
+
+}
